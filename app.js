eb2fa29f1dc8624df70eca96a016b1f7aee92315
ishulev <ivan.venelinov.shulev@gmail.com>
2021-08-09 16:10:14 +0300

    Good enough for basic

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const process = require("process");
 const Spinner = require("clui").Spinner;
 
-const { askInitialQuestion, filterByQuestion } = require("./lib/inquirer");
+const {
+  askInitialQuestion,
+  filterByQuestion,
+  listPeople,
+} = require("./lib/inquirer");
 const { EXIT, SEARCH, VIEW_ALL } = require("./lib/constants");
 const { getPeople, filterPeople } = require("./lib/query");
 
@@ -17,9 +21,9 @@ const spinner = new Spinner("Fetching people...  ", [
   "⣷",
 ]);
 
-function displayAllPeople() {
-  console.table(people, ["UserName", "FirstName", "LastName"]);
-}
+// function displayAllPeople() {
+//   console.table(people, ["UserName", "FirstName", "LastName"]);
+// }
 
 async function getAllPeople() {
   spinner.start();
@@ -29,7 +33,6 @@ async function getAllPeople() {
 
     if (response.hasOwnProperty("value")) {
       people = response.value;
-      displayAllPeople();
     } else {
       console.error("Response from server is not in expected format.");
     }
@@ -48,27 +51,33 @@ const run = async () => {
       const filterVal = await filterByQuestion();
       spinner.start();
       try {
-          const filteredPeople = await filterPeople(
-            filterVal.filterBy,
-            filterVal.filterValue
-          );
-          
+        const response = await filterPeople(
+          filterVal.filterBy,
+          filterVal.filterValue
+        );
+        spinner.stop();
+        if (response.value && response.value.length) {
+          const { displayPerson } = await listPeople(response.value);
+          console.log(displayPerson);
+        } else {
+          console.log("No people found!");
+        }
       } catch (error) {
-          console.error(error)
+        console.error(error);
       }
-      spinner.stop();
       break;
     case VIEW_ALL:
+      await getAllPeople();
       if (people.length) {
-        displayAllPeople();
-      } else {
-        await getAllPeople();
+        const selectedPerson = await listPeople(people);
+        console.log(selectedPerson);
       }
-      run();
       break;
     default:
       process.exit(1);
   }
+  console.log("-".repeat(10), "\n".repeat(3));
+  run();
 };
 
 run();
